7cf3b83f3f61a9169542d8af5bb9b1c77e69fb45
KuznetsovKU <kuznetsov_k@outlook.com>
2023-04-19 12:21:05 +0300

    added Homework6

--- src/main/java/org/example/Seminar3/Students/data/UserComparator.java
+++ src/main/java/org/example/Seminar3/Students/data/UserComparator.java
@@ -18,4 +18,30 @@ public class UserComparator<T extends User> implements Comparator<T> {
             return resultOfComparing;
         }
     }
+
+    public String compareWithDescription(T o1, T o2) {
+        int resultOfComparing = o1.getSecondName().compareTo(o2.getSecondName());
+        String description0 = "Согласно логике сравнения, объект " + o1.getMinInfo() + " равен объекту " +
+                o2.getMinInfo() + ". Объекты имеют одинаковые имя и фамилию";
+        String descriptionPos = "Согласно логике сравнения, объект " + o1.getMinInfo() + " меньше объекта " +
+                o2.getMinInfo() + ". В алфавитном списке объект " + o1.getMinInfo() + " встретится раньше объекта " +
+                o2.getMinInfo();
+        String descriptionNeg = "Согласно логике сравнения, объект " + o1.getMinInfo() + " больше объекта " +
+                o2.getMinInfo() + ". В алфавитном списке объект " + o1.getMinInfo() + " встретится позже объекта " +
+                o2.getMinInfo();
+        if (resultOfComparing == 0) {
+            resultOfComparing = o1.getFirstName().compareTo(o2.getFirstName());
+            if (resultOfComparing == 0) {
+                return description0;
+            } else if (resultOfComparing < 0) {
+                return descriptionNeg;
+            } else {
+                return descriptionPos;
+            }
+        } else if (resultOfComparing < 0) {
+            return descriptionNeg;
+        } else {
+            return descriptionPos;
+        }
+    }
 }
--- src/main/java/org/example/Seminar3/Students/service/UserService.java
+++ src/main/java/org/example/Seminar3/Students/service/UserService.java
@@ -1,9 +1,11 @@
 package org.example.Seminar3.Students.service;
 
+import org.example.Seminar3.Students.data.User;
+
 import java.time.LocalDate;
 import java.util.List;
 
-public interface UserService<T> {
+public interface UserService<T extends User> {
     List<T> getAll();
     void create(String firstName, String secondName, String patronymic, LocalDate dateOfBirth);
 }
